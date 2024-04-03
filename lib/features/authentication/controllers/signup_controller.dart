2c71fe358aefdb7fb12f4e167a3d75128a1b45fa
AhmadAkram52 <ahmadg03025249091@gmail.com>
2024-04-03 22:36:16 +0500

    UI Done

--- lib/features/authentication/controllers/signup_controller.dart
+++ lib/features/authentication/controllers/signup_controller.dart
@@ -3,4 +3,5 @@ import 'package:get/get.dart';
 
 class SignUpController extends GetxController {
   final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
 }
